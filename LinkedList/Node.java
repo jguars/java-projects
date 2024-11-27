65e2bdf1b1fa1aed37994f1ef46cc5a06939c0a0
Guvanch Jumayev <106640365+mrprogrmr@users.noreply.github.com>
2024-11-27 19:05:52 +0100

    added initial exercises from DSA

---
+++ LinkedList/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    String name;
+    String details;
+    Node next;
+
+    Node(String name, String details) {
+        this.name = name;
+        this.details = details;
+        this.next = null;
+    }
+}
