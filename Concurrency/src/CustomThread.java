3c85046f6928e3401ba64351c8eab94d9d1c6ac5
Guvanch Jumayev <gugalijumayew@gmail.com>
2024-12-10 15:43:03 +0100

    continue coding..

--- Concurrency/src/CustomThread.java
+++ Concurrency/src/CustomThread.java
@@ -1,2 +1,16 @@
-public class CustomThread {
+public class CustomThread extends Thread {
+
+//	@Override
+	public void run() {
+        System.out.println("Custom Thread is running");
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.print(" 1 ");
+			try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+		}
+	}
 }
--- Concurrency/src/Main.java
+++ Concurrency/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 	public static void main(String[] args) {
 		var currentThread = Thread.currentThread();
@@ -9,6 +11,34 @@ public class Main {
 		currentThread.setName("MainGuy");
 		currentThread.setPriority(Thread.MAX_PRIORITY);
 		printThreadState(currentThread);
+
+		CustomThread customThread = new CustomThread();
+		customThread.start();
+
+		Runnable runnableTask = () -> {
+
+			for (int i = 1; i <= 8; i++) {
+				System.out.print(" 2 ");
+				try {
+                    TimeUnit.MILLISECONDS.sleep(250);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+			}
+		};
+
+		Thread myThread = new Thread(runnableTask);
+		myThread.start();
+
+		for (int i = 1; i <= 3; i++) {
+			System.out.print(" 0 ");
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	public static void printThreadState(Thread thread) {
