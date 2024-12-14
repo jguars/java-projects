4b9d2bb39f509379fb807b02b32f6c8840cbd24c
Guvanch Jumayev <gugalijumayew@gmail.com>
2024-12-14 01:26:20 +0100

    continue coding..

---
+++ Hi-Low GUI App/src/GUITest.java
@@ -0,0 +1,20 @@
+public class GUITest {
+
+	//Simple GUI on Jshell: on Terminal, write
+	// <Jshell> to initialize the coding part
+	// write </edit> to open Jshell editor and
+	// write the below code
+
+
+//import javax.swing.*;
+//JFrame window = new JFrame("Guvanch's Window");
+//JPanel panel = new JPanel();
+//JButton button = new JButton("Click me!");
+//panel.add(button);
+//window.add(panel);
+//window.setSize(300,100);
+//button.addActionListener(e ->
+//			System.out.println("Ouch! You clicked me!"));
+//window.setVisible(true);
+
+}
